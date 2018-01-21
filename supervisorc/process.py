71f6a6e78e29b507ce68f3e3047cab988bd6012e
Eduard Carreras <ecarreras@gisce.net>
2018-01-21 16:43:39 +0100

    Fix _info attribute

--- supervisorc/process.py
+++ supervisorc/process.py
@@ -8,12 +8,13 @@ class Process(object):
         self.server = server
         self.supervisor = self.server.supervisor
         self._name = name
+        self._info = {}
         self.update_info()
 
     def update_info(self):
         """Internal method to update process information.
         """
-        self.info = self.supervisor.getProcessInfo(self._name)
+        self._info = self.supervisor.getProcessInfo(self._name).copy()
 
     def start(self, wait=True):
         """Start a this process.
@@ -48,94 +49,94 @@ class Process(object):
         Automatically information is updated by `self.update_info()`
         """
         self.update_info()
-        return self.info
+        return self._info
 
     @property
     def name(self):
         """The name of the process.
         """
         self.update_info()
-        return self.info['name']
+        return self._info['name']
 
     @property
     def group(self):
         """The group of the process if it is part of a group.
         """
         self.update_info()
-        return self.info['group']
+        return self._info['group']
 
     @property
     def description(self):
         """The description of the process.
         """
         self.update_info()
-        return self.info['description']
+        return self._info['description']
 
     @property
     def started(self):
         self.update_info()
-        return self.info['start']
+        return self._info['start']
 
     @property
     def stopped(self):
         self.update_info()
-        return self.info['stop']
+        return self._info['stop']
 
     @property
     def now(self):
         self.update_info()
-        return self.info['now']
+        return self._info['now']
 
     @property
     def state(self):
         """The state of the process (code).
         """
         self.update_info()
-        return self.info['state']
+        return self._info['state']
 
     @property
     def statename(self):
         """The state of the process (name).
         """
         self.update_info()
-        return self.info['statename']
+        return self._info['statename']
 
     @property
     def logfile(self):
         """The logfile of the process.
         """
         self.update_info()
-        return self.info['logfile']
+        return self._info['logfile']
 
     @property
     def stdout_logfile(self):
         """The log file of stdout.
         """
         self.update_info()
-        return self.info['stdout_logfile']
+        return self._info['stdout_logfile']
 
     @property
     def stderr_logfile(self):
         """The logfile of stderr.
         """
         self.update_info()
-        return self.info['stderr_logfile']
+        return self._info['stderr_logfile']
 
     @property
     def spawnerr(self):
         self.update_info()
-        return self.info['spawnerr']
+        return self._info['spawnerr']
 
     @property
     def exitstatus(self):
         """Status of the exit.
         """
         self.update_info()
-        return self.info['exitstatus']
+        return self._info['exitstatus']
 
     @property
     def pid(self):
         """The process identifier (pid).
         """
         self.update_info()
-        return self.info['pid']
+        return self._info['pid']
